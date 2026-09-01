82dea221a75023b1c661771e29f74f8e593f1933
AnAllergyToAnalogy <andrewdangerparker@gmail.com>
2026-09-01 11:39:41 +1000

    expose onEventDropout

--- src/lib/library/program.ts
+++ src/lib/library/program.ts
@@ -495,6 +495,7 @@ export async function createProgram(programClient: {[key: string]: any;}, idl: {
             killProgram,
 
             startSubscription: _subscribeToProgram,
+            onEventDropout,
 
             account,
 
